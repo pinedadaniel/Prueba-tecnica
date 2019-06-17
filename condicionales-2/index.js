f679ebd0404dbf65a52b23ccb780b11b56f47bf1
Sala 310 <sala310@poliacademico.edu.loc>
2019-06-17 12:35:51 -0500

    actualizado

---
+++ condicionales-2/index.js
@@ -0,0 +1,18 @@
+var metros = 2;
+var decimetros=0;
+var kilometros =0;
+
+if (metros % 2 == 0){
+    kilometros = metros/1000;
+
+console.log("kilometros " + kilometros)
+
+}else {
+    decimetros = metros * 10;
+    console.log("decimetros" + decimetros);
+
+}
+
+    
+
+
